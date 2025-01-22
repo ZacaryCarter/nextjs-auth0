f542627f6bb9149496b97051c2d82d052e91f7e4
Zacary Carter <c.zcarter@tdecu.org>
2025-01-22 15:53:26 +1100

    test

--- src/server/session/stateless-session-store.ts
+++ src/server/session/stateless-session-store.ts
@@ -80,7 +80,7 @@ export class StatelessSessionStore extends AbstractSessionStore {
     _reqCookies: cookies.RequestCookies,
     resCookies: cookies.ResponseCookies
   ) {
-    resCookies.set(this.SESSION_COOKIE_NAME, "", {
+    await resCookies.set(this.SESSION_COOKIE_NAME, "", {
       ...this.cookieConfig,
       maxAge: 0, // Set maxAge to 0 to delete the cookie
       expires: new Date(0), // Set expires to a past date
